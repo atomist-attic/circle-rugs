cae1568d82a3a9cb1b94d088b6abebb0f9b4ae27
slimslenderslacks <slimslenderslacks@gmail.com>
2017-04-24 16:48:45 -0700

    initial commit of circle handlers

---
+++ .atomist/handlers/event/Build.ts
@@ -0,0 +1,47 @@
+import { HandleEvent, EventPlan, LifecycleMessage, DirectedMessage, UserAddress } from '@atomist/rug/operations/Handlers'
+import { GraphNode, Match, PathExpression } from '@atomist/rug/tree/PathExpression'
+import { EventHandler, Tags } from '@atomist/rug/operations/Decorators'
+
+import { Build } from '@atomist/cortex/Build'
+
+@EventHandler("CircleCIBuilds", "Handle build events from CircleCI",
+    new PathExpression<Build, Build>(
+        `/Build
+            [@provider='circle']
+            [/commit::Commit()[/author::GitHubId()[/person::Person()/chatId::ChatId()]?]
+                [/tags::Tag()]?]
+                [/repo::Repo()/channels::ChatChannel()]
+            [/push::Push()
+                [/commits::Commit()/author::GitHubId()
+                    [/person::Person()/chatId::ChatId()]?]
+                [/repo::Repo()]]`))
+@Tags("ci", "circle")
+class Built implements HandleEvent<Build, Build> {
+    handle(event: Match<Build, Build>): EventPlan {
+        let build = event.root()
+        let plan = new EventPlan()
+
+        let repo = build.repo.name
+        let owner = build.repo.owner
+        let cid = "commit_event/" + owner + "/" + repo + "/" + build.commit.sha
+
+        let message = new LifecycleMessage(build, cid)
+
+        if (build.status == "failed" || build.status == "broken") {
+            try {
+                if (build.commit.author != null && build.commit.author.person != null) {
+                    let body = `Circle build \`#${build.name}\` of \`${owner}/${repo}\` failed after your last commit \`${build.commit.sha}\`: ${build.buildUrl}`
+                    let address = new UserAddress(build.commit.author.person.chatId.id)
+                    plan.add(new DirectedMessage(body, address))
+                }
+            }
+            catch (e) {
+                console.log((<Error>e).message)
+            }
+        }
+
+        plan.add(message)
+        return plan
+    }
+}
+export const built = new Built()
